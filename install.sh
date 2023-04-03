e98c0b09fcc3b8e4565ef3dce9038adf9214b60c
12TreeMan31 <theinvisiblebench2@gmail.com>
2023-04-03 14:29:39 -0500

    Inital commit

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Script for installing the needed packages
+
+pkgs=(
+    "xorg"
+    "xorg-xinit"
+    "i3"
+    "demu"
+    "xterm"
+    "gtk3"
+    "flameshot"
+    "noto-fonts"
+    "noto-fonts-cjk"
+    "noto-fonts-emoji"
+)
+
+for i in "${pkgs[@]}"
+do
+   pacman -S --needed --noconfirm $i
+done
+
+cp ./dotfiles/i3-config ~/.config/i3/config
+cp ./dotfiles/flameshot.config ~/.config/flameshot/flameshot.ini
+cp ./dotfiles/gtk3-config ~/.config/gtk-3.0/settings.ini
+
