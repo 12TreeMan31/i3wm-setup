7ad20d392d8769a4a2704589e4f7fc4a8df04b1f
12TreeMan31 <theinvisiblebench2@gmail.com>
2023-04-03 14:42:50 -0500

    Inital Commit

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ pkgs=(
     "i3"
     "demu"
     "xterm"
+    "feh"
     "gtk3"
     "flameshot"
     "noto-fonts"
@@ -23,4 +24,6 @@ done
 cp ./dotfiles/i3-config ~/.config/i3/config
 cp ./dotfiles/flameshot.config ~/.config/flameshot/flameshot.ini
 cp ./dotfiles/gtk3-config ~/.config/gtk-3.0/settings.ini
-
+cp ./dotfiles/bashrc ~/.bashrc
+cp ./dotfiles/xinitrc ~/.xinitrc
+cp ./dotfiles/Xresources ~/.Xresources
